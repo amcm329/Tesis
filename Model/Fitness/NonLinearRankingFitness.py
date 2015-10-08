5601ec9308be640d551438e9ca54099df67814d9
Aaron <aaron@DebianMimoni>
2015-10-07 19:22:05 -0500

    Compu T

--- Model/Fitness/NonLinearRankingFitness.py
+++ Model/Fitness/NonLinearRankingFitness.py
@@ -4,7 +4,7 @@
 #Version 2.0
 #Author: Castillo Medina Aarón Martín.
 
-def deriv(polynome):
+def derivate(polynome):
     for index in range(len(polynome)):
             polynome[index]*=index
     
@@ -27,36 +27,39 @@ def calculate_root(polynome,x_0,epsilon):
     for number  in polynome:
             polynome_1.append(number)    
     
-    deriv(polynome_1)
+    derivate(polynome_1)
     while abs(value) >= epsilon:
-        derivalue=(evaluate_polynome(polynome_1,x_0))
+        deriv_value=(evaluate_polynome(polynome_1,x_0))
         if derivalue == 0:
             x_0 = 0.0
             break
         
-        x_0 = x_0 - (value/derivalue)
+        x_0 = x_0 - (value/deriv_value)
         value=(evaluate_polynome(polynome,x_0))
        
     return x_0
             
 
 #Orden ascendente para que pueda hacerse bien el ranking.
+#Aqui no estoy seguro si es en orden ascendente o descendente
 def assign_non_linear_ranking_fitness(population,fitness_options):
-    total_fitness = 0 0 
+    total_fitness = 0.0 
     sp = fitness_options[0] 
+    population_size = population.get_size()
     population.sort_individuals(self,"get_pareto_dominants",False)
-
-    polynome = [sp*population.get_length()]
-
-    for x in range (,-1)
     
+    #Creating polynome
+    polynome = [sp - population_size]
 
+    for x in range (population_size - 1,-1,-1):
+        polynome.append(sp)
+    
     solution = calculate_root(polynome,0.0,0.0000001)         
-
+    y = 0
     for individual in population.get_individuals():
-        current_fitness = 
+        current_fitness = (population_size*(solution**y))/ 
         individual.set_fitness(current_fitness)
-        total_fitness += current_fitness        
+        total_fitness += current_fitness        l
         y += 1
   
     population.set_total_fitness(total_fitness) 
